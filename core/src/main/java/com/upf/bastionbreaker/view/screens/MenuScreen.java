266f4d17f72998218286df4c0c4cbb279aa1ee82
plegrand2710 <pauline.legrand2710@gmail.com>
2025-03-07 02:51:34 -1000

    GUI - création du splashScreen

--- core/src/main/java/com/upf/bastionbreaker/view/screens/MenuScreen.java
+++ core/src/main/java/com/upf/bastionbreaker/view/screens/MenuScreen.java
@@ -1,4 +1,53 @@
 package com.upf.bastionbreaker.view.screens;
 
-public class MenuScreen {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MenuScreen implements Screen {
+    private Game game;
+    private SpriteBatch batch;
+    private TextureAtlas menuAtlas;
+    private TextureRegion background;
+
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        menuAtlas = new TextureAtlas(Gdx.files.internal("menu.atlas"));
+        background = menuAtlas.findRegion("menu_background");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        menuAtlas.dispose();
+    }
 }
